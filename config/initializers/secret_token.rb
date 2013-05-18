b83dcabefcdbf3488f2a0a7e8d110ae4ac13d330
Takuya Noaki <nokkii.03@gmail.com>
2013-05-18 10:07:06 +0900

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsAdventure::Application.config.secret_token = 'bcc6c37d12aed5ade078304456bc5ad5806daf2f75c6d3d77180f9f46d56bc6be30bbb4e53b60e1bd0ff0e0b27e64e04c05b8b3cf860291907860a3d5ec8acf9'
