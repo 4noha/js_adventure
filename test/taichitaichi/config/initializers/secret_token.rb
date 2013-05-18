edc6e3f5c4f34aebb848a7cb313bfc7c49f56c8d
yakuzarashi <yakuzarashi@gmail.com>
2013-05-18 12:30:45 +0900

    rails初期設定

---
+++ test/taichitaichi/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taichitaichi::Application.config.secret_token = 'ba954633ff8adbb45863235ee5fbd5ba2c44a5c30732d96e7441fa3f3b5d2796324b4708f95de1a5504c8d32bbb3a575c265c2b35cb083d66cefc8cced036dc7'
